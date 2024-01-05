a0fbc845d404f25316d4bc0c1651d33da4372ed5
evgeshkins <kidixcomp@mail.ru>
2024-01-05 20:47:53 +0500

    Mainscreen: base design for test completed

--- app/src/main/java/com/example/weatherappproject/MainScreen.kt
+++ app/src/main/java/com/example/weatherappproject/MainScreen.kt
@@ -7,9 +7,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -19,6 +28,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
     Surface(
@@ -41,6 +51,49 @@ fun MainScreen() {
                     .padding(top = 50.dp)
                     .fillMaxWidth()
                     .wrapContentSize(Alignment.Center)
+            )
+
+            var textValue by remember { mutableStateOf("") }
+
+            OutlinedTextField(
+                value = textValue,
+                onValueChange = { newTextValue ->
+                    textValue = newTextValue  // Обновление переменной с текстовым значением при вводе
+                },
+                label = { Text(text = stringResource(id = R.string.hint_user_name)) },
+                modifier = Modifier
+                    .padding(top = 100.dp)
+                    .fillMaxWidth()
+                    .wrapContentWidth(Alignment.CenterHorizontally),
+                textStyle = MaterialTheme.typography.bodyLarge,
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    textColor = colorResource(id = R.color.text_color),
+                    focusedBorderColor = Color.Black,
+                    unfocusedBorderColor = Color.Black
+                )
+            )
+
+            Button(
+                onClick = { /* Handle button click */ },
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .fillMaxWidth()
+                    .wrapContentWidth(Alignment.CenterHorizontally)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.main_btn),
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+            Text(
+                text = "test",
+                modifier = Modifier
+                    .padding(top = 30.dp)
+                    .fillMaxWidth()
+                    .wrapContentWidth(Alignment.CenterHorizontally),
+                color = colorResource(id = R.color.text_color),
+                fontSize = 25.sp
             )
         }
     }
